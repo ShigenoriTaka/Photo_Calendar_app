eab7aac0d65e02e359949cdacf5bfe3c19970859
chige <shige053@me.com>
2019-03-06 01:30:43 +0900

    Spinner実装中

--- app/src/main/java/com/example/shigenoritakahashi/photo_calendar_app2/App_tool_Event.java
+++ app/src/main/java/com/example/shigenoritakahashi/photo_calendar_app2/App_tool_Event.java
@@ -12,27 +12,26 @@ public class App_tool_Event {
     private TextView Text_tool_Ev_1;
     private TextView Text_tool_Ev_2;
 
+    private String text_tool_Ev_1[] = {"2017", "2018", "2019", "2020"};
+
+    private String text_tool_Ev_2[] = {
+            "January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December"};
+
+    private TextView textView;
+
+
+
+
+
     public void init(TextView text_tool_Ev_1, TextView text_tool_Ev_2) {
         Text_tool_Ev_1 = text_tool_Ev_1;
         Text_tool_Ev_2 = text_tool_Ev_2;
 
     }
 
-    public  void Text_tool_EV(Calendar calendar) {
-        int year = calendar.get(Calendar.YEAR);
-        Text_tool_Ev_1.setText(String.valueOf(year));
 
-        int month = calendar.get(Calendar.MONTH);
-        String monthName_Ev = theMonth_Ev(month);
-        Text_tool_Ev_2.setText(String.valueOf(monthName_Ev));
 
-    }
-
-    public static String theMonth_Ev(int month) {
-        String[] monthNAme_EV = {"January", "February", "March", "April", "May", "June", "July",
-                "August", "September", "October", "November", "December"};
-        return monthNAme_EV[month];
-    }
 
 
 
